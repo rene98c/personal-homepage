f20810695fd8464657255a46fb8e89c4a4497a98
rene <rene@bdec.ee>
2025-04-12 07:33:26 +0300

    cleanup

--- src/components/UI/ContactMethod.tsx
+++
@@ -1,8 +0,0 @@
-export default function ContactMethod() {
-  return (
-    <div>
-      <h2>ContactMethod</h2>
-      <p>In progress</p>
-    </div>
-  );
-} 
--- src/components/UI/JobExperience.tsx
+++
@@ -1,8 +0,0 @@
-export default function JobExperience() {
-  return (
-    <div>
-      <h2>JobExperience</h2>
-      <p>In progress</p>
-    </div>
-  );
-} 
