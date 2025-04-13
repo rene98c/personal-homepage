a59e1b7012d30642e9bf503f2f97f05830fb4189
rene <rene@bdec.ee>
2025-04-13 11:50:26 +0300

    localization support

---
+++ next-i18next.config.js
@@ -0,0 +1,10 @@
+// next-i18next.config.js
+module.exports = {
+    i18n: {
+      defaultLocale: 'en',
+      locales: ['en', 'et'],
+      localeDetection: true,
+    },
+    defaultNS: 'common',
+    localePath: './public/locales',
+  }
