5dfd64e9080cd32e9ef4aec91c7096f7a63a75a2
Rene Prost <rene@bdec.ee>
2025-04-15 07:28:13 +0300

    wrong date

--- src/components/blog/posts/MewsIntegrationPost.tsx
+++ src/components/blog/posts/MewsIntegrationPost.tsx
@@ -81,7 +81,7 @@ export const MewsIntegrationPost = ({
             <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd"></path>
             </svg>
-            April 20, 2025
+            April 15, 2025
           </span>
           <span className="mx-2">•</span>
           <span>Integration • AI • Development</span>
